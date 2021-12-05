cf61190050990664ef23a05af4032b26db9eb440
jsneuman <jsneuman2@gmail.com>
2021-12-05 00:07:58 -0500

    Created template

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -1,5 +1,17 @@
+// Jacob Neuman
+// 12/4/2021
+// CIS 1202 501
+
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+template <typename T>
+T half(T whole)
+{
+	T half = whole / 2;
+	return half;
+}
 
 int main()
 {
