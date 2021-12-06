944ed50ca6a54ca329dfafc183a4ece6408d856f
jsneuman <jsneuman2@gmail.com>
2021-12-05 23:52:45 -0500

    bug fixes

--- Templates/Templates.cpp
+++ Templates/Templates.cpp
@@ -10,10 +10,25 @@ template <typename T>
 T half(T whole)
 {
 	T half = whole / 2;
+
+	return half;
+}
+
+int half(int whole)
+{
+	int half = round(static_cast<float>(whole) / 2);
 	return half;
 }
 
 int main()
 {
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
+
 	return 0;
 }
